46eb2f7a95622a679832dfb9e105952a7b75235e
meAgitHUBuser <163905570+meAgitHUBuser@users.noreply.github.com>
2024-06-30 21:54:34 +0530

    Update KnapsackGreedy.java

--- src/KnapsackGreedy.java
+++ src/KnapsackGreedy.java
@@ -1,69 +1,64 @@
 import java.util.Scanner;
 
 public class KnapsackGreedy {
-     public static void main(String[] args) {
-          int n, maxCapacity;
-          float totalProfit = 0;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-          Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of items: ");
+        int n = scanner.nextInt();
 
-          System.out.println("Enter the number of items: ");
-          n = scanner.nextInt();
+        int[] weights = new int[n];
+        int[] values = new int[n];
 
-          int[] weights = new int[n];
-          int[] values = new int[n];
+        System.out.println("Enter the weights of the items: ");
+        for (int i = 0; i < n; i++) {
+            weights[i] = scanner.nextInt();
+        }
 
-          System.out.println("Enter the weights of the items: ");
-          for (int i = 0; i < n; i++) {
-               weights[i] = scanner.nextInt();
-          }
+        System.out.println("Enter the values of the items: ");
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
 
-          System.out.println("Enter the values of the items: ");
-          for (int i = 0; i < n; i++) {
-               values[i] = scanner.nextInt();
-          }
+        System.out.print("Enter the maximum capacity of the knapsack: ");
+        int maxCapacity = scanner.nextInt();
 
-          System.out.println("Enter the maximum capacity of the knapsack: ");
-          maxCapacity = scanner.nextInt();
+        float[] ratios = new float[n];
+        for (int i = 0; i < n; i++) {
+            ratios[i] = (float) values[i] / weights[i];
+        }
 
-          int remainingCapacity = maxCapacity;
+        float totalProfit = 0;
+        int remainingCapacity = maxCapacity;
 
-          float[][] itemRatio = new float[n][2];
-          for (int i = 0; i < n; i++) {
-               itemRatio[i][0] = (float) values[i] / weights[i];
-               itemRatio[i][1] = i;
-          }
+        while (remainingCapacity > 0) {
+            float maxRatio = 0;
+            int selectedItemIndex = -1;
 
-          while (remainingCapacity > 0) {
-               float maxRatio = 0;
-               int selectedItemIndex = -1;
+            for (int i = 0; i < n; i++) {
+                if (weights[i] > 0 && ratios[i] > maxRatio) {
+                    maxRatio = ratios[i];
+                    selectedItemIndex = i;
+                }
+            }
 
-               for (int i = 0; i < n; i++) {
-                    if (itemRatio[i][0] > maxRatio && values[(int) itemRatio[i][1]] > 0) {
-                         maxRatio = itemRatio[i][0];
-                         selectedItemIndex = (int) itemRatio[i][1];
-                    }
-               }
+            if (selectedItemIndex == -1) {
+                break;
+            }
 
-               if (selectedItemIndex == -1) {
-                    break;
-               }
+            if (weights[selectedItemIndex] <= remainingCapacity) {
+                System.out.println("Item " + (selectedItemIndex + 1) + " added.");
+                remainingCapacity -= weights[selectedItemIndex];
+                totalProfit += values[selectedItemIndex];
+                weights[selectedItemIndex] = 0; // Mark the item as used
+            } else {
+                System.out.println("Partial quantity of item " + (selectedItemIndex + 1) + " added.");
+                totalProfit += remainingCapacity * ratios[selectedItemIndex];
+                remainingCapacity = 0;
+            }
+        }
 
-               if (weights[selectedItemIndex] > remainingCapacity) {
-                    System.out.println(
-                              "Quantity of item number: " + (selectedItemIndex + 1) + " added is " + remainingCapacity);
-                    totalProfit += remainingCapacity * maxRatio;
-                    remainingCapacity = 0;
-               } else {
-                    System.out.println("Quantity of item number: " + (selectedItemIndex + 1) + " added is "
-                              + weights[selectedItemIndex]);
-                    remainingCapacity -= weights[selectedItemIndex];
-                    totalProfit += values[selectedItemIndex];
-                    values[selectedItemIndex] = 0;
-               }
-          }
-
-          System.out.println("The total profit is " + totalProfit);
-          scanner.close();
-     }
+        System.out.println("Total profit: " + totalProfit);
+        scanner.close();
+    }
 }
